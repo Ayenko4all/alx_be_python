607af0185007eb14d7d069b4f8a5597895f8d611
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2025-11-08 19:06:09 +0100

    Add basic finance calculator script
    Introduces a script to calculate monthly savings and projected annual savings with interest based on user input for income and expenses.

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+monthly_income = int(input("Enter your monthly income: "))
+total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - total_monthly_expenses
+print(f"Your monthly savings are: {monthly_savings}")
+interest_rate = 0.05
+Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * interest_rate)
+print(f"Projected savings after one year, with interest is: {Projected_savings}")
