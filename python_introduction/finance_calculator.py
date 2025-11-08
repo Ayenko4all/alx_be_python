d71f02a32f72ed78e161b368cf99bbb4415f1e97
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2025-11-08 19:09:57 +0100

    Refactor variable names and output formatting
    Standardized variable naming to snake_case and improved print statements for clarity in finance_calculator.py.

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,9 @@
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
-monthly_savings = monthly_income - total_monthly_expenses
-print(f"Your monthly savings are: {monthly_savings}")
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+print(f"Your monthly savings are {monthly_savings}")
+
 interest_rate = 0.05
-Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * interest_rate)
-print(f"Projected savings after one year, with interest is: {Projected_savings}")
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * interest_rate)
+print(f"Projected savings after one year, with interest is: {projected_savings}")
