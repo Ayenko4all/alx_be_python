81408cf51682f7fa710ad3631b64666c6e8fec24
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2025-12-07 12:27:02 +0100

    Add shopping list manager script
    Introduces a command-line shopping list manager that allows users to add, remove, and view items in a shopping list, as well as exit the program.

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,38 @@
+shopping_list = []
+
+while True:
+    print("\nShopping List Manager")
+    print("1. Add item")
+    print("2. Remove item")
+    print("3. View list")
+    print("4. Exit")
+
+    choice = input("Choose an option (1-4): ")
+
+    if choice == "1":
+        item = input("Enter item to add: ")
+        shopping_list.append(item)
+        print(f"{item} added to the shopping list.")
+
+    elif choice == "2":
+        item = input("Enter item to remove: ")
+        if item in shopping_list:
+            shopping_list.remove(item)
+            print(f"{item} removed from the shopping list.")
+        else:
+            print(f"{item} not found in the shopping list.")
+
+    elif choice == "3":
+        if not shopping_list:
+            print("Shopping list is empty.")
+        else:
+            print("Current shopping list:")
+            for item in shopping_list:
+                print(f"- {item}")
+
+    elif choice == "4":
+        print("Exiting Shopping List Manager.")
+        break
+
+    else:
+        print("Invalid choice. Please select a valid option.")
